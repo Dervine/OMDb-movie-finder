8c2a472782401d40cbaa9217f5e690e2079b328c
Dervine <dervine@Dervines-MacBook-Pro.local>
2023-11-03 10:31:21 +0300

    debug 500 error

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -14,6 +14,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
+    console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
